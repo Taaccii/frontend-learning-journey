8ff92e3a00031469ac0b704a7018443f24873b24
Taaccii <tom1991@hotmail.it>
2026-02-12 16:24:59 +0100

    add some new exercise for DOM manipolation

--- week-04-array-dom-javascript/es-dom/es-dom.js
+++ week-04-array-dom-javascript/es-dom/es-dom.js
@@ -39,3 +39,21 @@ divParagraph.textContent = "Me Too!";
 content.appendChild(divParagraph);
 
 document.body.appendChild(content);
+
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function (e) {
+  e.target.style.background = "blue";
+});
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
+
